4d6f314d4d9ccb36e8ad88b6e93f5b348bad5655
Steve <github@cavender.io>
2026-05-29 15:08:02 -0400

    add knip lsp to nvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Knip language server (https://github.com/webpro-nl/knip/tree/main/packages/language-server)
+-- Not yet in nvim-lspconfig/Mason; configured via native vim.lsp API.
+-- Must run after VeryLazy (vim_did_enter=1) so vim.lsp.enable() fires doautoall for open buffers.
+vim.lsp.config("knip", {
+  cmd = { "npx", "--yes", "@knip/language-server", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  root_markers = { "package.json", "knip.json", "knip.jsonc" },
+})
+vim.lsp.enable("knip")
